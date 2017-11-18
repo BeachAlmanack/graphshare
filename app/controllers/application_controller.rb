422dcbd2c960f24570f5708d20c551d63f2bbc6e
Julien Gurunlian <jgurunlian@me.com>
2017-11-18 15:48:23 -0800

    Create User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  helper_method: current_user
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= user.find_by(session_token: session[:session_token])
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
