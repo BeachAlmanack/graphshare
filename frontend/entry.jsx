a0f887d0f58d361b4508fec8b4d523c6cdf993fd
Julien Gurunlian <jgurunlian@me.com>
2017-11-18 16:52:58 -0800

    Create API calls for users creation and login

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { postSession, postUser, deleteSession } from './utils/api/users_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<div> Hello World </div>, root);
 });
+
+window.postSession = postSession;
+window.deleteSession = deleteSession;
