2dc4f5b39d6d4729e8bb6d96a301e28f894e5fa6
Julien Gurunlian <jgurunlian@me.com>
2017-11-28 18:00:35 -0800

    Clear state on logout

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,9 +2,18 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import entitiesReducer from './entities/entities_reducer';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
-export default combineReducers({
+const combinedReducers = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
 });
+
+
+export default (state, action) => {
+  if (action.type === LOGOUT_CURRENT_USER) {
+    state = undefined
+  }
+  return combinedReducers(state, action);
+};
