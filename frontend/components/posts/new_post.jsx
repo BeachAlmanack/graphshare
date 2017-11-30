5a1eb1cc2f413a58cc0c8ce1065664f90202444a
Julien Gurunlian <jgurunlian@me.com>
2017-11-30 15:46:50 -0800

    Add error handling for new post

--- frontend/components/posts/new_post.jsx
+++ frontend/components/posts/new_post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { omit } from 'lodash';
 
 class NewPost extends React.Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class NewPost extends React.Component {
       description: '',
       postable_type: this.props.contentType,
       postable_id: this.props.contentId,
+      error: '',
     };
     this.handleUpdate = this.handleUpdate.bind(this);
     this.saveNewPost = this.saveNewPost.bind(this);
@@ -22,8 +24,15 @@ class NewPost extends React.Component {
   }
 
   saveNewPost(event) {
-    event.preventDefault()
-    this.props.savePost(this.state).then(() => this.props.history.push('/feed'));
+    event.preventDefault();
+    if (!this.state.title) {
+      this.setState({
+        error: 'You need a title to post',
+      });
+      return;
+    }
+
+    this.props.savePost(omit(this.state, 'error')).then(() => this.props.history.push('/feed'));
   }
 
   render() {
@@ -31,12 +40,13 @@ class NewPost extends React.Component {
     return (
       <div className="new-post">
         <h2>New Post</h2>
+        <p className="error">{ this.state.error ? this.state.error : ''}</p>
         <form className="form">
           <label htmlFor="title"> Post title:
             <input id="title" type="text" placeholder="title" value={this.state.title} onChange={this.handleUpdate('title')} />
           </label>
           <label htmlFor="description"> Description:
-            <textarea name="" id="description" cols="30" rows="5" value={this.state.description} onChange={this.handleUpdate('description')} />
+            <textarea placeholder="(optional)" name="" id="description" cols="30" rows="5" value={this.state.description} onChange={this.handleUpdate('description')} />
           </label>
           <div className="save">
             <button className="button-green" onClick={this.saveNewPost}> Save </button>
