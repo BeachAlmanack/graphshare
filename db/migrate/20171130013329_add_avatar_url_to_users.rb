4d3e4da759a9dc59e440ce80cd5000db3481f35a
Julien Gurunlian <jgurunlian@me.com>
2017-12-01 00:20:20 -0800

    Update store package.json and webpack config for prod

--- db/migrate/20171130013329_add_avatar_url_to_users.rb
+++ db/migrate/20171130013329_add_avatar_url_to_users.rb
@@ -1,5 +1,5 @@
 class AddAvatarUrlToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :avatar_url, :string, default: 'http://res.cloudinary.com/dunj0n6sb/image/upload/v1512006039/0_m4syrq.svg'
+    add_column :users, :avatar_url, :string, default: 'https://res.cloudinary.com/dunj0n6sb/image/upload/v1512006039/0_m4syrq.svg'
   end
 end
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
 const path = require('path');
+const webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    },
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    },
+  }),
+];
+
+plugins = plugins.concat(process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins);
 
 module.exports = {
   entry: './frontend/entry.jsx',
@@ -6,6 +24,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js',
   },
+  plugins,
   module: {
     loaders: [
       {
